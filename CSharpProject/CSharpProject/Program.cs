4e8cf6154ed87c1893f73a4d961a6b75d3333a4a
vicdant1 <dantjv@outlook.com>
2022-01-28 16:13:37 -0300

    virtual methods and override

--- CSharpProject/CSharpProject/Program.cs
+++ CSharpProject/CSharpProject/Program.cs
@@ -4,109 +4,161 @@
     {
         Console.WriteLine("============* Starting Program *============\n");
 
-        var genericValueInt = new Generic<int>();
-        genericValueInt.Add(13);
+        #region Virtual Override
+            //var human = new Human();
+        
+            //human.Fight();
+            //human.Die();
+        #endregion
+        
+        #region Generics
+            /*
 
-        var genericValueString = new Generic<string>();
-        genericValueString.Add("string value in the same method");
+            var genericValueInt = new Generic<int>();
+            genericValueInt.Add(13);
+
+            var genericValueString = new Generic<string>();
+            genericValueString.Add("string value in the same method");*/
+        #endregion
+
 
     }
 
-    public class Generic<T>
-    {
-        public void Add(T anyTypeValue)
-        {
-            Console.WriteLine("Type of <T> working. Value: \n" + anyTypeValue);
-        }
-    }
-
-    // using generics constraints
-    /*
-     I can use generics in a method or in a whole class
-     */
-    public class GenConstraints
-    {
-
-        /* Useful constraints
-         * where T : IComparable
-         * where T : class
-         * where T : Product (for instance)
-         * where T : new() -> newly declareted obj
+    #region Virtual Override
+        /* Virtual methods are declared in the mother class
+         * Only virtual methods can be overrided
+         * You can use virtual methods as they were declared or override them
+         * they CANNOT be private
+         * A overrided method is already a virtual method to it's next generations (inheritance)
          */
 
-        /* 
-        
-        This way it will NOT work, because comp doesn't know what the type of a or b
-
-        public T Compare<T>(T a, T b)
+    public class Animal 
+    {     
+        public virtual void Fight()
         {
-            a > b ? a : b;
-            return null;
+            Console.WriteLine("Animals are fighting");
         }
 
-        to solve this problem, we have constraints, which are tools to specify some rules to that
-        generic operation
-
-        */
-
-        public T CompareMax<T>(T a, T b) where T : IComparable
+        public virtual void Die()
         {
-            return a.CompareTo(b) > 0 ? a : b;
+            Console.WriteLine("Animals are dying");
         }
     }
 
-    /* 
-        It is completly fine to write a generic method inside of a no generic class
+    public class Human : Animal
+    {
+        // overriding the default method
 
-        In the most part of the cases, I'll use this on repository pattern building generic methods for crud
+        public override void Fight()
+        {
+            Console.WriteLine("I'm a human and I fight with weapons");
+        }
 
-        generic class example:
+        // I wont override the Die() method
+    }
 
-        IBaseRepo:
-        // methods to add, update, delete and save
-        public interface IBaseRepo 
+
+    #endregion
+
+    #region Generics
+
+        public class Generic<T>
+        {
+            public void Add(T anyTypeValue)
+            {
+                Console.WriteLine("Type of <T> working. Value: \n" + anyTypeValue);
+            }
+        }
+
+        // using generics constraints
+        /*
+         I can use generics in a method or in a whole class
+         */
+        public class GenConstraints
         {
 
-            public void Add<T>(T entity) where T : class;
-            public void Update<T>(T entity) where T : class;
-            public void Delete<T>(T entity) where T : class;
+            /* Useful constraints
+             * where T : IComparable
+             * where T : class
+             * where T : Product (for instance)
+             * where T : new() -> newly declareted obj
+             */
 
-            public Task<bool> SaveAsyncChanges();
+            /* 
+        
+            This way it will NOT work, because comp doesn't know what the type of a or b
 
+            public T Compare<T>(T a, T b)
+            {
+                a > b ? a : b;
+                return null;
+            }
+
+            to solve this problem, we have constraints, which are tools to specify some rules to that
+            generic operation
+
+            */
+
+            public T CompareMax<T>(T a, T b) where T : IComparable
+            {
+                return a.CompareTo(b) > 0 ? a : b;
+            }
         }
+
+        /* 
+            It is completly fine to write a generic method inside of a no generic class
+
+            In the most part of the cases, I'll use this on repository pattern building generic methods for crud
+
+            generic class example:
+
+            IBaseRepo:
+            // methods to add, update, delete and save
+            public interface IBaseRepo 
+            {
+
+                public void Add<T>(T entity) where T : class;
+                public void Update<T>(T entity) where T : class;
+                public void Delete<T>(T entity) where T : class;
+
+                public Task<bool> SaveAsyncChanges();
+
+            }
      
-        BaseRepo:
+            BaseRepo:
 
-        public class BaseRepo : IBaseRepo
-        {
-            private readonly AppDbContext _dbContext;
-
-            public BaseRepo(AppDbContext dbContext)
+            public class BaseRepo : IBaseRepo
             {
-                _dbContext = dbContext;
-            }
+                private readonly AppDbContext _dbContext;
 
-            public void Add<T>(T entity) where T : class;
-            {
-                _dbContext.Add(entity);
-            }
+                public BaseRepo(AppDbContext dbContext)
+                {
+                    _dbContext = dbContext;
+                }
+
+                public void Add<T>(T entity) where T : class;
+                {
+                    _dbContext.Add(entity);
+                }
             
-            public void Update<T>(T entity) where T : class;
-            {
-                _dbContext.Update(entity);
-            }
+                public void Update<T>(T entity) where T : class;
+                {
+                    _dbContext.Update(entity);
+                }
     
-            public void Delete<T>(T entity) where T : class;
-            {
-                _dbContext.Delete(entity);
-            }
+                public void Delete<T>(T entity) where T : class;
+                {
+                    _dbContext.Delete(entity);
+                }
             
-            public Task<bool> SaveAsyncChanges();
-            {
-                return await _dbContext.SaveAsyncChanges() > 0;
-            }    
-        }
-     */
+                public Task<bool> SaveAsyncChanges();
+                {
+                    return await _dbContext.SaveAsyncChanges() > 0;
+                }    
+            }
+         */
+
+    #endregion
 
 
 }
