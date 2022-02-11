57e79f1ecb6f298583e0199b3d370d7b4e043cac
vicdant1 <dantjv@outlook.com>
2022-02-11 14:17:09 -0300

    studying type lib and adding sass to the project

--- webdesign-study/animations/type_mod.js
+++ webdesign-study/animations/type_mod.js
@@ -2,11 +2,22 @@
 // ` word ` -> wont type this word
 
 var typed = new Typed('.element', {
-  strings: ['Vue.js', 'React.js', 'Microsoft^500 .NET', 'git push --force ^1000 `pushed to origin with option force`'],
+  strings: ['Vue.js', 'React.js', 'Microsoft^500 .NET', 'git push --force ^1000\n `pushed to origin with option force`'],
   typeSpeed: 30,
+  smartBackspace: true, // 'til same letters in the word, ex: Microsoft and Micronesia -> it will backspace til 'micro'
+  // shuffle: true,
+  // fadeOut: true, -> fade instead of use backspace
+  loop: true,
+
   /*
   
   explore docs https://github.com/mattboldt/typed.js
 
   */
 });
+
+
+var typed2 = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  loop: true,
+}); 
