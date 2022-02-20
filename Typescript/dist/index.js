ca9bf459db213f8d0c6c0348da386091c87c6ef1
victor.dantas <dantjv@outlook.com>
2022-02-19 23:35:58 -0300

    studying typescript fundamentals

--- Typescript/dist/index.js
+++ Typescript/dist/index.js
@@ -4,3 +4,42 @@ let id = 5;
 let company = "Traversy Media Inc";
 let isPublished = true;
 let x = "Hello";
+// if u set x to anything it will not throw an error
+x = 3;
+x = "hello again";
+// Arrays
+let ids = [1, 2, 3, 4, 5];
+// ids.push("Hello world"); -> it'll complain
+let arrAny = ["hello", 123, true];
+// Tuple
+let person = [1, "brad"];
+// array of tuple
+let employees;
+employees = [
+    [1, "brad"],
+    [2, "pitt"],
+    [3, "abluble"]
+];
+// if some order has been broken, it will throw an error for sure
+// union -> mixing types
+let product_id;
+product_id = 22;
+product_id = "333";
+//Enum
+var DirectionOne;
+(function (DirectionOne) {
+    // values from zero to x
+    DirectionOne[DirectionOne["Up"] = 1] = "Up";
+    DirectionOne[DirectionOne["Down"] = 2] = "Down";
+    DirectionOne[DirectionOne["Left"] = 3] = "Left";
+    DirectionOne[DirectionOne["Right"] = 4] = "Right";
+})(DirectionOne || (DirectionOne = {}));
+console.log(DirectionOne.Down);
+var DirectionTwo;
+(function (DirectionTwo) {
+    DirectionTwo["Up"] = "Up.txt";
+    DirectionTwo["Down"] = "Down.txt";
+    DirectionTwo["Left"] = "Left.txt";
+    DirectionTwo["Right"] = "Right.txt";
+})(DirectionTwo || (DirectionTwo = {}));
+console.log(DirectionTwo.Down);
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -6,3 +6,78 @@ let company:string = "Traversy Media Inc";
 let isPublished:boolean = true;
 let x:any = "Hello";
 
+// if u set x to anything it will not throw an error
+
+x = 3;
+x = "hello again";
+
+// Arrays
+
+let ids:number[] = [1,2,3,4,5];
+// ids.push("Hello world"); -> it'll complain
+let arrAny:any[] = ["hello", 123, true];
+
+// Tuple
+
+let person:[number, string] = [1, "brad"];
+
+// array of tuple
+
+let employees:[number, string][];
+
+employees = [
+  [1, "brad"],
+  [2, "pitt"],
+  [3, "abluble"]
+];
+
+// if some order has been broken, it will throw an error for sure
+
+// union -> mixing types
+
+let product_id:number|string;
+
+product_id = 22;
+product_id = "333";
+
+//Enum
+
+enum DirectionOne{
+  // values from zero to x
+  Up = 1,
+  Down,
+  Left,
+  Right
+}
+
+console.log(DirectionOne.Down);
+
+enum DirectionTwo{
+  Up = "Up.txt",
+  Down = "Down.txt",
+  Left = "Left.txt",
+  Right = "Right.txt"
+}
+
+console.log(DirectionTwo.Down);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
