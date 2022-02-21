d215778f6955c828476fc9d2d132b53199fa2dc2
victor.dantas <dantjv@outlook.com>
2022-02-21 20:49:37 -0300

    studying context api in typescript

---
+++ Typescript/ts-react-app/src/context/user.tsx
@@ -0,0 +1,49 @@
+import React, {createContext, useContext, useState} from "react";
+
+type UserType = {
+  name:string,
+  email:string,
+  password:string
+};
+
+type PropsUserContext = {
+  state:UserType,
+  setState:React.Dispatch<React.SetStateAction<UserType>>
+};
+
+const contextDefaultValue = {
+  state:{
+    name:"",
+    email:"",
+    password:""
+  },
+  setState:() => {}
+};
+
+const UserContext = createContext<PropsUserContext>(contextDefaultValue);
+
+// sera exportado para envolver a aplicacao
+export const UserContextProvider:React.FC = ({children}) => {
+  const [state, setState] = useState(contextDefaultValue.state)
+  return(
+    <UserContext.Provider value={{state, setState}}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export const useUserContext = () => {
+  const context = useContext(UserContext);
+}
+
+
+
+
+
+
+
+
+
+
+
+
