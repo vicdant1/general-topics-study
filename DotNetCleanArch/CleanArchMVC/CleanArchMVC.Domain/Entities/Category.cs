be7ff1327e952d81aa32cdc561a53b0d647aa74a
vicdant1 <dantjv@outlook.com>
2022-03-19 16:23:37 -0300

    creating anemic models

---
+++ DotNetCleanArch/CleanArchMVC/CleanArchMVC.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchMVC.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ DotNetCleanArch/CleanArchMVC/CleanArchMVC.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanArchMVC.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
