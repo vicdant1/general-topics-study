8aef5f4cf2f6d9d70a90abb581f15caf99d6b62b
vicdant1 <dantjv@outlook.com>
2022-03-19 17:47:25 -0300

    adding repositories interfaces

---
+++ DotNetCleanArch/CleanArchMVC/CleanArchMVC.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMVC.Domain.Entities;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetByIdAsync(int? id);
+
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+    }
+}
---
+++ DotNetCleanArch/CleanArchMVC/CleanArchMVC.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchMVC.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+
+        Task<Product> GetProductCategoryAsync(int? id);
+
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
