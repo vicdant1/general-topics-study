0b5411658ee47739d14df6c4eba2ba83305b7a9f
vicdant1 <dantjv@outlook.com>
2022-03-19 17:07:44 -0300

    adding valuable validations to models following ddd

--- DotNetCleanArch/CleanArchMVC/CleanArchMVC.Domain/Entities/Category.cs
+++ DotNetCleanArch/CleanArchMVC/CleanArchMVC.Domain/Entities/Category.cs
@@ -1,10 +1,36 @@
-﻿namespace CleanArchMVC.Domain.Entities
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
+﻿using CleanArchMVC.Domain.Validation;
 
-        public ICollection<Product> Products { get; set; }
+namespace CleanArchMVC.Domain.Entities
+{
+    public sealed class Category : EntityBase
+    {
+        public string Name { get; private set; }
+        public ICollection<Product> Products { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3, "Name should have at least 3 letters");
+
+            Name = name;
+        }
     }
 }
--- DotNetCleanArch/CleanArchMVC/CleanArchMVC.Domain/Entities/Product.cs
+++ DotNetCleanArch/CleanArchMVC/CleanArchMVC.Domain/Entities/Product.cs
@@ -1,15 +1,54 @@
-﻿namespace CleanArchMVC.Domain.Entities
+﻿using CleanArchMVC.Domain.Validation;
+
+namespace CleanArchMVC.Domain.Entities
 {
-    public class Product
+    public sealed class Product : EntityBase
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public string Image { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
         public int CategoryId { get; set; }
         public Category Category { get; set; }
 
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id");
+            Id = id;
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
+        }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Name should have at least 3 letters");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Description is required");
+            DomainExceptionValidation.When(description.Length < 5, "Description should have at least 5 letters");
+
+            DomainExceptionValidation.When(price < 0, "Invalid price value");
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value");
+
+            DomainExceptionValidation.When(image.Length > 250, "Too long image name, maximum 250 characters");
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
     }
 }
