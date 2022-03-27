b96cf1a90b7254c7aa10917912c33cec5bde3f5b
João Victor Dantas <dantjv@outlook.com>
2022-03-27 20:40:46 -0300

    studying and applying vuejs concepts (pre esigOS19)

--- Vue/VueJs/changer/src/App.vue
+++ Vue/VueJs/changer/src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="text-center text-success">changER</h1>
+    <h5 class="text-center">South american countries</h5>
+
+    <div class="row mt-4">
+      <div class="col-md-4" v-for="country in countries" :key="country.name">
+        <CountryCard>
+          <template v-slot:title>
+            {{country.name}}
+          </template>
+          <template v-slot:capital>
+            {{country.capital}}
+          </template>
+          <template v-slot:language>
+            {{country.languages.map(lang => ` ${lang.name}`).toString()}}
+          </template>
+          <template v-slot:currencie>
+            {{country.currencies.map(curr => curr.name).toString()}}
+          </template>
+          <template v-slot:flag>
+            <img :src="country.flags.svg" :alt="country.name">
+          </template>
+        </CountryCard>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountryCard from './components/CountryCard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CountryCard,
+  },
+  data(){
+    return{
+      countries: []
+    }
+  },
+
+  methods: {
+    async fetchCountriesData(){
+      const data = await
+        fetch("https://restcountries.com/v2/regionalbloc/USAN")
+        .then(res => res.json());
+      this.countries = data
+    }
+  },
+  mounted(){
+    this.fetchCountriesData();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ Vue/VueJs/changer/src/components/CountryCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="cardContainer">
+        <div class="textContainer">
+            <h6 class="title">
+                <slot name="title"/>
+            </h6>
+            Capital: <slot name="capital"/><br/>
+            Language(s): <slot name="language"/><br/>
+            Currencie: <slot name="currencie"/><br/>
+        </div>
+        <slot name="flag"/>
+    </div>    
+</template>
+
+<script>
+export default{
+    name: 'CountryCard',
+}
+
+</script>
+
+<style scoped>
+
+*{
+    color: rgb(131, 131, 131);
+}
+
+.cardContainer{
+    width: 100%;
+    min-height: 140px;
+    border-radius: 4px;
+    background-color: #FFF;
+    position: relative;
+    padding: .5rem;
+    transition: .4s transform;
+    box-shadow: 1px 5px 5px rgba(0, 0, 0, 0.3);
+    margin-bottom: 15px;
+}
+
+.cardContainer:hover{
+    transform: scale(1.1);
+}
+
+.cardContainer img{
+    position: absolute;
+    width: 35px;
+    top: 3px;
+    right: 3px;
+    border-radius: 3px;
+}
+</style>
