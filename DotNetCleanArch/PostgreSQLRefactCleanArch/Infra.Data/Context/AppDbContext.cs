4280b460de11d96160718300fcd58383459bd1ca
João Victor Dantas <dantjv@outlook.com>
2022-04-19 22:23:17 -0300

    working on datacontext

---
+++ DotNetCleanArch/PostgreSQLRefactCleanArch/Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Database=PGCleanArch;Username=vicdant1;Password=0508");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
