14d9e575077c9742b781908faa140fd283d6b9d8
João Victor Dantas <dantjv@outlook.com>
2022-04-23 19:13:29 -0300

    creating first migration

--- php/laravel/codeWithDary/firstproject/routes/web.php
+++ php/laravel/codeWithDary/firstproject/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use App\Http\Controllers\PagesController;
 
 Route::get('/', [PagesController::class, 'index']);
 Route::get('/about', [PagesController::class, 'about']);
+
+Route::get('/posts', [PostsController::class, 'index']);
