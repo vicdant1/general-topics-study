df32ad250892502240d02821414b06ef6c57708d
João Victor Dantas <dantjv@outlook.com>
2022-04-23 12:05:28 -0300

    passing data directly to the view

--- php/laravel/codeWithDary/firstproject/app/Http/Controllers/ProductsController.php
+++ php/laravel/codeWithDary/firstproject/app/Http/Controllers/ProductsController.php
@@ -10,18 +10,22 @@ class ProductsController extends Controller
     {
         $title = "Welcome to my Laravel website";
         $description = "Created by Jay";
-        
-        // Compact method
-        // return view('products.index', compact('title', 'description'));
-
-        // With method
         $data = [
             1 => 'iPhone',
             2 => 'Samsung',
             3 => 'BlackBerry'
         ];
+        
+        // Compact method
+        // return view('products.index', compact('title', 'description'));
 
-        return view('products.index')->with('data', $data);
+        // With method
+        // return view('products.index')->with('data', $data);
+
+        // Directly to the view
+        return view('products.index', [
+            'data' => $data
+        ]);
 
     }
 
