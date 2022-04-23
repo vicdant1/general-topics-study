907b9fb5c1ca8381f00690b50d3150dff2f01a47
João Victor Dantas <dantjv@outlook.com>
2022-04-23 14:25:48 -0300

    adding images to project

--- php/laravel/codeWithDary/firstproject/resources/views/index.blade.php
+++ php/laravel/codeWithDary/firstproject/resources/views/index.blade.php
@@ -17,7 +17,7 @@
 <!-- Every div is a grid item -->
 <div class="container-grid-4">
     <div>
-        <img src="img/icon-box.jpg" alt="">
+        <img src="{{ URL('/images/icon-box.jpg') }}" alt="">
         <h2>
             Web Design
         </h2>
@@ -27,7 +27,7 @@
     </div>
 
     <div>
-        <img src="img/icon-box.jpg" alt="">
+        <img src="{{ URL('/images/icon-box.jpg') }}" alt="">
         <h2>
             Web Development
         </h2>
@@ -37,7 +37,7 @@
     </div>
 
     <div>
-        <img src="img/icon-box.jpg" alt="">
+        <img src="{{ URL('/images/icon-box.jpg') }}" alt="">
         <h2>
             Product Design
         </h2>
@@ -47,7 +47,7 @@
     </div>
 
     <div>
-        <img src="img/icon-box.jpg" alt="">
+        <img src="{{ URL('/images/icon-box.jpg') }}" alt="">
         <h2>
             Creative Thinker
         </h2>
@@ -66,7 +66,7 @@
 <!-- Meet the team section -->
 <div class="container-grid-2">
     <div>
-        <img class="img-team" src="img/team-section-home.jpg" alt="">
+        <img class="img-team" src="{{ URL('images/team-section-home.jpg') }}" alt="">
     </div>
     <div>
         <h2>
