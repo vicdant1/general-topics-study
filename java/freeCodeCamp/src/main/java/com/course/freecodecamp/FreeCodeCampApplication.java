741f209d4529f94e9db52a229f80b9500defe24f
João Victor Dantas <dantjv@outlook.com>
2022-05-04 19:33:55 -0300

    scaffolding java web api

---
+++ java/freeCodeCamp/src/main/java/com/course/freecodecamp/FreeCodeCampApplication.java
@@ -0,0 +1,13 @@
+package com.course.freecodecamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeCodeCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeCodeCampApplication.class, args);
+	}
+
+}
