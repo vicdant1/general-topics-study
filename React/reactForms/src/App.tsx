92b9cdbf4ab8384c0105a0ee42da8bca16648e39
João Victor Dantas <dantjv@outlook.com>
2022-05-06 15:21:55 -0300

    studying useMemo and remembering some react concepts

--- React/reactForms/src/App.tsx
+++ React/reactForms/src/App.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import BasicForm from "./components/BasicForm";
 import Counter from "./components/Counter";
 
 const App = () => {
   const [showCounter, setShowCounter] = useState(false);
+  const [randomInputState, setRandomInputState] = useState("");
+
+  const calcBoolean = useMemo(() => {
+    console.log("function called");
+    if (showCounter == true) return "TRUE";
+    else return "FALSE";
+  }, [showCounter]);
 
   return (
     <>
@@ -19,15 +26,24 @@ const App = () => {
       <h4 className="text-center mt-2">
         Study ReactHooks and Context Stuff(ContextAPI || Redux)
       </h4>
-
       <button
         onClick={() => setShowCounter(!showCounter)}
         className="btn btn-primary"
       >
-        Show Counter
+        {!showCounter ? "Show" : "Close"} Counter
       </button>
-
       {showCounter && <Counter />}
+      <hr />
+      calcBoolean does not depends on any other state change than showCounter,
+      so we could memo this value and just listen to showCounter changes. <br />
+      {calcBoolean}
+      <input
+        type="text"
+        name="random"
+        id="random"
+        onChange={(e) => setRandomInputState(e.target.value)}
+      />
+      <br />
     </>
   );
 };
