b89aa60eca7d20c91d76a8227e3154a61c407b7a
vicdant1 <dantjv@outlook.com>
2022-05-25 18:41:41 -0300

    working with dynamic url params

--- Node/express/express-crash/Members.js
+++ Node/express/express-crash/Members.js
@@ -1,26 +1,31 @@
 
 const members = [
   {
+    id: 0,
     name: 'John',
     age: 21,
     height: 171
   },
   {
+    id: 1,
     name: 'Michael',
     age: 12,
     height: 132
   },
   {
+    id: 2,
     name: 'John K.',
     age: 61,
     height: 181
   },
   {
+    id: 3,
     name: 'Alezs Khaickv',
     age: 20,
     height: 191
   },
   {
+    id: 4,
     name: 'John V',
     age: 21,
     height: 170
--- Node/express/express-crash/index.js
+++ Node/express/express-crash/index.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const path = require("path");
-const members = require('./Members')
-const InitialMiddleware = require('./InitialMiddleware');
+const members = require("./Members");
+const InitialMiddleware = require("./InitialMiddleware");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 // setting information as json
-app.get('/api/members', (req, res) => {
+app.get("/api/members", (req, res) => {
   res.json(members);
 });
 
-app.get('/api/members/:id', (req, res) => {
-  const {id} = req.params;
-  res.send(id); 
-});
+app.get("/api/members/:id", (req, res) => {
+  const { id } = req.params;
+  const filteredMembers = members.filter(
+    (member) => member.id === parseInt(id)
+  );
 
+  filteredMembers.length > 0
+    ? res.status(200).json(filteredMembers)
+    : res.status(400).json({ msg: "not found :/" });
+});
 
 // middlewares
 // app.use(InitialMiddleware);
 // creating a statc dir to the application
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => console.log(`app running at ${PORT} port.`));
