218d71f7ad7690793e6c3c7b23375d40c4473322
vicdant1 <dantjv@outlook.com>
2022-05-26 18:14:20 -0300

    removing .env

---
+++ Node/express/express-study-api/config/dbconnection.js
@@ -0,0 +1,6 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+});
