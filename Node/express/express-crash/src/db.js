2c5f5e82f93c1b24731d138d12897a0ac4b9cb0a
vicdant1 <dantjv@outlook.com>
2022-05-26 18:10:42 -0300

    scaffolding project

--- Node/express/express-crash/src/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
-  host: process.env.DATABASE_HOST,
-  dialect: process.env.DATABASE_DIALECT
-});
-
-
-module.exports = sequelize;
---
+++ Node/express/express-study-api/index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+// process.env.NODE_ENV;
+
+const Express = require("express");
+const app = new Express();
+
