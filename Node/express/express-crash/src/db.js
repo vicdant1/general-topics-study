71adb218486f1876a78d833a618796ed346cc9cc
vicdant1 <dantjv@outlook.com>
2022-05-25 22:59:18 -0300

    database validation

--- Node/express/express-crash/src/db.js
+++ Node/express/express-crash/src/db.js
@@ -1,6 +1,3 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
   host: process.env.DATABASE_HOST,
--- Node/express/express-crash/src/index.js
+++ Node/express/express-crash/src/index.js
@@ -5,6 +5,10 @@ dotenv.config();
 
 const sequelize = require("./db");
 
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+});
+
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server is running on port ${process.env.SERVER_PORT}`);
