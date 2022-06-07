cb27cd3f2b4ef27168e996ca590acd716dc3f5fa
vicdant1 <dantjv@outlook.com>
2022-06-06 22:26:45 -0300

    recreating login screen

--- Nextjs/sapere-initial/pages/login/index.tsx
+++ Nextjs/sapere-initial/pages/login/index.tsx
@@ -12,45 +12,49 @@ const Login: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="row">
-        <div className={`col-md-7 ${style.imgContainer} p-0 d-none d-md-block`}>
-          {/* <img src="/images/Avalon.png" alt="login-image" /> */}
+      <div className="row p-relative">
+        <div className={style.floatingBrand}>
+          <Image
+            src="/images/AvalonShortLogo.svg"
+            width={40}
+            height={40}
+          />
         </div>
-        <div className="col-md-5 m-0 p-0 bg-white">
-          <div
-            className="d-flex justify-content-center align-items-center flex-column"
-            style={{ height: "100vh" }}
-          >
-            <h3 className="text-center">Bem vindo à Avalon</h3>
+
+        <div
+          className={`col-md-7 d-none d-md-block ${style.sideImageContainer}`}
+        ></div>
+
+        <div className={`col-md-5 bg-white d-flex flex-column justify-content-center align-items-center ${style.loginContainer}`}>
+          <div className={`d-flex flex-column align-items-center justify-content-center ${style.loginFormContainer}`}>
+            <h3 className="text-center display-6">Bem vindo à Avalon</h3>
             <p className="font-weight-light text-muted">
               Faça login em sua conta
             </p>
 
             <form className={style.formLogin}>
               <div className="form-group">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  aria-describedby="emailHelp"
-                  placeholder="Usuário"
-                />
-
-                <input
-                  type="password"
-                  className="form-control mt-2"
-                  id="password"
-                  aria-describedby="passwordHelp"
-                  placeholder="Senha"
-                />
-
-                <div className="d-flex justify-content-end mt-2">
-                  <a href="http://google.com">Esqueci minha senha</a>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    aria-describedby="emailHelp"
+                    placeholder="Usuário"
+                  />
+                  <input
+                    type="password"
+                    className="form-control mt-2"
+                    id="password"
+                    aria-describedby="passwordHelp"
+                    placeholder="Senha"
+                  />
+                  <div className="d-flex justify-content-end mt-2">
+                    <a href="http://google.com">Esqueci minha senha</a>
+                  </div>
                 </div>
-              </div>
-              <Link href="/">
-                <button className="btn btn-dark mt-2 w-100">Login</button>
-              </Link>
+                <Link href="/">
+                  <button className="btn btn-dark mt-2 w-100">Login</button>
+                </Link>
             </form>
           </div>
         </div>
